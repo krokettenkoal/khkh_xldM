a6d6e9f17cc4cbad157476c90915c351c9f25b2b
Cajetan Grill <mt201096@fhstp.ac.at>
2022-08-24 01:51:24 +0200

    Update comments

--- khkh_xldMii_slim/Form1.cs
+++ khkh_xldMii_slim/Form1.cs
@@ -11,15 +11,17 @@ namespace khkh_xldMii_slim {
         }
 
         private void btnOpenMdlx_Click(object sender, EventArgs e) {
-            using OpenFileDialog openFileDialog = new OpenFileDialog();
+            //  Show an 'Open file' dialog to the user for them to select an MDLX file
+            using OpenFileDialog openFileDialog = new();
             openFileDialog.InitialDirectory = @"C:\gamedev\Assets\Kingdom Hearts\test";
             openFileDialog.Filter = "MDLX files (*.mdlx)|*.mdlx|All files (*.*)|*.*";
             openFileDialog.FilterIndex = 2;
             openFileDialog.RestoreDirectory = true;
 
             if (openFileDialog.ShowDialog() == DialogResult.OK) {
-                //Get the path of specified file
+                //  Get the path of specified file
                 mdlxPath = openFileDialog.FileName;
+
                 Log($"{mdlxPath} opened.");
             }
         }
@@ -32,15 +34,26 @@ namespace khkh_xldMii_slim {
 
             Log("Exporting ASET..");
 
-            var success = MdlxConvert.ToAset(mdlxPath, slimDxPanel.Handle, Log);
+            //  Export an ASET animation file from the specified MDLX file.
+            //  A corresponding MSET file is required in the same directory as the MDLX file.
+            var success = MdlxConvert.ToAset(mdlxPath, Log);
 
             Log($"Export done. Success: {success}");
         }
 
+        /// <summary>
+        /// Print a text line to the log panel
+        /// </summary>
+        /// <param name="msg">The message to print</param>
         private void Log(string msg) {
             txtLog.AppendText(msg + "\r\n");
         }
 
+        /// <summary>
+        /// Print the export progress consisting of an ExportState and an ExportStatus to the log panel.
+        /// </summary>
+        /// <param name="state">State (stage) of the current export operation</param>
+        /// <param name="status">Status (anim/frame info) of the current export operation</param>
         private void Log(ExportState state, ExportStatus status) {
             Log($"[{state}] {status.animName}");
         }
